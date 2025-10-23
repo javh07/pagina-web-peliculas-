7837e398bef5791bd791a660e90cfe2979a56f88
javh07 <joseantonio.varela@uan.edu.mx>
2025-10-23 12:34:52 -0700

    Coneccion con nodejs

---
+++ blog/blog_backend/database/connection.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const dbConnection = async() => {   
+    try {
+        await mongoose.connect( "mongodb+srv://joseantoniovarela_db_user:IUEAEK25VTLl3pTp@blog.albrkm3.mongodb.net/?retryWrites=true&w=majority&appName=blog");
+        console.log('Base de datos online');  
+    }
+
+    catch (error) {
+        console.log(error);
+        throw new Error('Error a la hora de iniciar la base de datos');
+    }   
+
+}
+
+module.exports = {
+    dbConnection
+}
---
+++ blog/blog_backend/index.js
@@ -0,0 +1,40 @@
+const { dbConnection } = require("./database/connection.js");
+const express = require ("express");
+const cors = require("cors");
+
+
+dbConnection();
+
+console.log("hola mundo");
+
+//Crear servidor Node
+const app = express ();
+const puerto = 3900;
+
+//Configurar cors
+app.use( cors());
+
+//Convertir body a objeto js
+app.use( express.json());
+
+//Rutas
+app.get("/probando", (req, res) => {
+    
+    console.log("Ruta probando funcionando");
+
+    return res.status(200).json([{
+        curso : "Master en React",
+        autor : "Jose Antonio Varela",
+        url : "joseantoniovarela.com"
+        
+        
+    }]);
+
+
+});
+
+//Crear servidor y escuchar peticiones http
+app.listen(puerto, () => {
+    console.log("Servidor corriendo en el puerto" + puerto);
+
+});
